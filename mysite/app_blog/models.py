22651c1a618d8c92eefcad0a535820eb56361761
John Doe <savchyn.yevhen@ukd.edu.ua>
2025-04-12 11:42:24 +0300

    django-server-side-development-pr5.

--- mysite/app_blog/models.py
+++ mysite/app_blog/models.py
@@ -4,19 +4,27 @@ from django.urls import reverse
  
  # Create your models here.
 class Category(models.Model):
-     category = models.CharField(
-         u'Категорія',
-         max_length=250,
-         help_text=u'Максимум 250 символів'
-     )
-     slug = models.SlugField(u'Слаг')
- 
-     class Meta:
-         verbose_name = u'Категорія для публікації'
-         verbose_name_plural = u'Категорії для публікацій'
- 
-     def __str__(self):
-         return self.category
+      category = models.CharField(u'Категорія',
+      max_length=250, help_text=u'Максимум 250 сим.')
+      slug = models.SlugField(u'Слаг')
+      objects = models.Manager()
+
+
+class Meta:
+        verbose_name = u'Категорія для публікації'
+        verbose_name_plural = u'Категорії для публікацій'
+
+
+def __str__(self):
+        return self.category
+def get_absolute_url(self):
+    try:
+         url = reverse('articles-category-list',
+                       kwargs={'slug': self.slug})
+    except:
+         url = "/"
+         return url
+
  
  
 class Article(models.Model):
